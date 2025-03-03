49c040bb5509f30cc9d89d03f6b8b6d7d6b0b0d3
xingshen <yibeiyou99@gmail.com>
2025-03-03 16:25:15 +0800

    移动国际化文本的位置。

--- packages-private/seren-viewer-develop/vite.config.js
+++ packages-private/seren-viewer-develop/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-  },
+    build: {
+      outDir: 'dist', // 指定输出目录
+      assetsDir: 'assets', // 指定静态资源目录
+      sourcemap: true, // 是否生成 sourcemap
+    },
 });
